74e6e02c5464c6b0abc4582d5a587c002596eea9
Varun Sreedharan <varunfloppa@Varuns-MacBook-Pro.local>
2023-02-24 20:15:53 -0800

    weird movement

--- snake.py
+++ snake.py
@@ -1,19 +1,20 @@
 import pygame
+from random import randint
 from sys import exit
 
 pygame.init()
-window = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Snake')
+window = pygame.display.set_mode((800, 480))
+pygame.display.set_caption('Snake-Python')
 clock = pygame.time.Clock()
 game_active = True
 
-snakebody = pygame.Rect(300, 300, 32, 32)
-velocity = 2
+velocity = 16
 x = 0
 y = 0 
 
-food = pygame.Rect(150, 150, 20, 20)
-
+food = pygame.Rect(150, 150, 8, 8)
+snakecoords = [[240, 400]]
+snakelen = 3
 pixel_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 while True:
@@ -39,34 +40,38 @@ while True:
             y = velocity
             x = 0
         
-        if snakebody.y - y > 0 and snakebody.y + y < (600-32):
-            snakebody.y += y
+        if snakecoords[0][1] - y > 0 and snakecoords[0][1] + y < (480-16):
+            snakecoords[0][1] += y
         else:
             game_active = False
 
-        if snakebody.x - x > 0 and snakebody.x + x < (600-32):
-            snakebody.x += x
+        if snakecoords[0][0] - x > 0 and snakecoords[0][0] + x < (800-16):
+            snakecoords[0][0] += x
         else:
             game_active = False
+        
+        print(f'{x}x, {y}y')
 
         window.fill((20, 20, 50))
-        pygame.draw.rect(window, (0, 200, 0), snakebody)
-        
+
+        for i in range(snakelen):
+            pygame.draw.rect(window, (0, 200, 0), pygame.Rect(snakecoords[0][0] + (i*x), snakecoords[0][1] + (i*y), 16, 16))
+
     else:
         window.fill((50, 20, 20))
         message = pixel_font.render('You lost at snake', False, (255, 255, 255))
-        message_rect = message.get_rect(center = (300, 275))
+        message_rect = message.get_rect(center = (400, 200))
         messagetwo = pixel_font.render('press space to start again', False, (255, 255, 255))
-        messagetwo_rect = messagetwo.get_rect(center = (300, 325))
+        messagetwo_rect = messagetwo.get_rect(center = (400, 250))
         window.blit(message, message_rect)
         window.blit(messagetwo, messagetwo_rect)
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_SPACE]:
-            snakebody.y = 300
-            snakebody.x = 300
+            snakecoords[0][1] = 240
+            snakecoords[0][0] = 400
             game_active = True
     
     # draw to the screen
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(16)
